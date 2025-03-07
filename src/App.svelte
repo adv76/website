f84755bcc8264b18af570897f06f4a524fdb0c5a
adv <alecdevries66@gmail.com>
2025-03-06 22:58:14 -0500

    Initial Commit
    Set up Vite / Svelte project and added tailwindcss

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+
+</script>
+
+<header class="bg-zinc-900 min-h-svh flex flex-col justify-end p-8">
+    <h1 class="font-serif text-8xl text-zinc-50">Hello World</h1>
+</header>
+
+<main>
+
+</main>
+
+<footer>
+
+</footer>
