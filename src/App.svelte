1a41407dfcc68c737d7d4a381afa24d8a977b73f
adv <alecdevries66@gmail.com>
2025-03-19 22:22:30 -0400

    Added some flag images

--- src/App.svelte
+++ src/App.svelte
@@ -3,9 +3,14 @@
     import LocomotiveScroll from "locomotive-scroll";
     import { onMount } from "svelte";
 
+    import americanFlag from "./assets/american_flag.png";
+    import appealFlag from "./assets/appeal_to_heaven.png";
+    import moultrieFlag from "./assets/fort_moultrie_flag.png";
+    import sonsLibertyFlag from "./assets/sons_of_liberty_flag.png";
+
     onMount(() => {
         const scroll = new LocomotiveScroll({
-            el: document.querySelector("[data-scroll-container]"),
+            el: document.querySelector("[data-scroll-container]") as HTMLElement,
             smooth: true
         });
 
@@ -19,10 +24,20 @@
             &#127482&#127480
         </span>
 
+        
+        
+
         <h1 class="font-serif text-8xl text-zinc-50">Hello World</h1>
     </header>
     
     <main class="font-serif text-zinc-50 text-xl p-8 space-y-8 mb-16">
+        <div class="grid grid-cols-4 gap-4 mb-8">
+            <img data-scroll data-scroll-delay="0.1" data-scroll-speed="6" src={sonsLibertyFlag} alt="Sons of Liberty Flag" width="1920" height="1009" class="rounded" />
+            <img data-scroll data-scroll-delay="0.3" data-scroll-speed="6" src={appealFlag} alt="American Flag" width="1920" height="1009" class="rounded translate-y-4" />
+            <img data-scroll data-scroll-delay="0.5" data-scroll-speed="6" src={moultrieFlag} alt="Fort Moultrie Flag" width="1920" height="1377" class="rounded -translate-y-4" />
+            <img data-scroll data-scroll-delay="0.7" data-scroll-speed="6" src={americanFlag} alt="American Flag" width="1920" height="1009" class="rounded translate-y-8" />
+        </div>
+
         <div class="p-8 bg-red-500 rounded size-32 translate-x-full" data-scroll data-scroll-direction="horizontal" data-scroll-speed="36"></div>
 
         <p data-scroll data-scroll-delay="0.1" data-scroll-speed="6">Non vel tenetur et. Est quod et ea amet suscipit maxime. Iste animi ipsam pariatur. Et consectetur maxime voluptas aut harum. Voluptas a quia amet doloremque ut et nihil aliquid.</p>
